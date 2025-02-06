fc6319438e998c49e4b86273c7c6d64e0ddeb544
Gabriel Berezin <gabrielberezin123@gmail.com>
2025-02-06 10:27:44 +0000

    Task 1

--- Program.cs
+++ Program.cs
@@ -8,8 +8,23 @@ namespace lecture2
 {
     internal class Program
     {
+        static void Task1(int n)
+        {
+            for (int i = 1; i <= 1.5 * n; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("\n\n");
+            for (int i = 0; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
         static void Main(string[] args)
         {
-        }
+            Task1(2);
+            Task1(4);
+            Task1(6);
+        }1
     }
 }
